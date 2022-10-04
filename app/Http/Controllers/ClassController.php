638866ed31952b9c46e24a6aa477cd1c7aa5eca2
Mohammed Saad <eng.mohameds2016@gmail.com>
2022-10-04 09:42:19 +0200

    edit share leasson

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -32,14 +32,15 @@ class ClassController extends Controller
 
             $students = ClassesStudent::with('student')->where([['class_id',$class_id],['teacher_id' , $teacher_id ]])->get();
 
-            $content = '<div class="form-group col-md-3 student_'.$div_id.'"><label>Students </label></div>' ;
-            $content .= '<div class="form-group col-md-9 form1 student_'.$div_id.'" >' ;
-            $content .= '<select class="form-control select2"  multiple name="class_students[]" required>' ;
+            // $content = '<div class="form-group col-md-3 student_'.$div_id.'"><label>Students </label></div>' ;
+            // $content .= '<div class="form-group col-md-9 form1 student_'.$div_id.'" >' ;
+            $content = '<select class="form-control select2"  multiple name="class_students[]" required>' ;
             foreach($students as $item)
             {
-                $content .= '<option value="'.$class_id.'_'.$item->student->id.'">'.$item->student->fname . ' ' .$item->student->lname.'</option>' ; 
+                $content .= '<option value="'.$class_id.'_'.$item->student->id.'" selected="selected">'.$item->student->fname . ' ' .$item->student->lname.'</option>' ; 
             }
             $content .= '</select></div>' ;
+            // $content .= '</div>' ;
             echo $content ;
             exit ;
         }
@@ -53,14 +54,35 @@ class ClassController extends Controller
             $lesson     = Lessons::findOrfail($lesson_id);
             $classes    = Classes::where([['instructor_id' , $lesson->instructor_id] , ['grade_id' , $lesson->grade ]])->get() ;
             $rand_id = rand('1111','9999') ;
-            $content = '<div class="form-group col-md-3 form1"><label>Choose Class </label></div><div class="accordion col-md-9 form1" >' ;
-            $content .= '<select class="choosedClass" id="'.$rand_id.'" name="class_id[]" required >' ;
-            $content .= '<option value="">Choose Class</option>' ;
+
+            $content = '<div class="shadow rounded p-3" id="main_'.$rand_id.'">
+                        <div class="chooseClass">
+                                <div class="form-group row">
+                                    <label for="lessonName" class="col-md-3 col-form-label">Choose Class</label>
+                                    <div class="col-md-7"><select class="form-control choosedClass" id="'.$rand_id.'" required><option value="">Choose Class</option>';
+                                        
             foreach($classes as $item)
             {
                 $content .= '<option value="'.$item->id.'">'.$item->name.'</option>' ; 
             }
-            $content .= '</select></div>' ;
+            $content .= '</select></div>
+                            <div class=" col-md-2">
+                                <div class="lessonBtn">
+                                    <i class="fa fa-eye mr-2 show" status="1" id="'.$rand_id.'" style="cursor: pointer" aria-hidden="true"></i>
+                                    <i class="fa fa-minus deleteDivClass" id="'.$rand_id.'" style="cursor: pointer" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="students_'.$rand_id.'" style="display:none;">
+                            <div class="form-group row w-100 d-flex">
+                                <label for="lessonName" class="col-md-3 col-form-label">Students</label>
+                                <div class="col-sm-9" id="student_'.$rand_id.'">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        </div>' ;
             echo $content;
             exit;  
         }
--- resources/views/instructor/classes/share.blade.php
+++ resources/views/instructor/classes/share.blade.php
@@ -1,11 +1,9 @@
 @extends('instructor.layouts.head')
 @section('title','Share the lessons')
 @section('maincontent')
-<div class="Become ">
+<div class=" ">
     <div class="custom-container">
         <div class="formTe bg-white">
-            <h4> Share the lessons </h4>
-
             @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -18,119 +16,130 @@
             @if(Session::has('success'))
             <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('success') }}</p>
             @endif
-
-            <form method="post" action="{{route('saveShare')}}" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                {{ method_field('post') }}
-                <div class="form-row " id="shareLesson">
-                    <div class="form-group col-md-3 form1">
-                        <label>Lesson Name </label>
-                    </div>
-                    <div class="col-md-9">
-                        <div class="form-group  form1">
-                            <div class="d-flex justify-content-between">
-                                <div class="lessonName">
-                                    <input type="hidden" type="text" id="lesson_id" value="{{$lesson->id}}"
-                                        class="form-control" name="lesson_id">
-                                    {{$lesson->name}}
-                                </div>
-                                <div class="lessonBtn">
-                                    <button class="btn ">
-                                        <i class="fa fa-eye" aria-hidden="true"></i>
-                                    </button>
-                                    <button class="btn">
-                                        <i class="fa fa-minus" aria-hidden="true"></i>
-                                    </button>
-
-                                </div>
+            <div class="shareLesson mt-5">
+                <div class="container bg-white rounded py-4">
+                    <h4 class="h4 text-primary"> Share the lessons </h4>
+                    <form method="post" action="{{route('saveShare')}}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        {{ method_field('post') }}
+                        <!-- begin Lesson Name -->
+                        <div class="form-group row">
+                            <label for="lessonName" class="col-md-3 col-form-label">Lesson Name</label>
+                            <div class="col-sm-9 d-flex align-content-center">
+                                <span> {{$lesson->name}} </span>
+                                <input type="hidden" id="lesson_id" value="{{$lesson->id}}" class="form-control" name="lesson_id">
                             </div>
                         </div>
-                    </div>
-
-                    @if(count($share) > 0)
-                    @foreach($classes as $class)
-                    @if(count($class->sharelesson) > 0)
-                    <div class="shadow p-3 mb-5 rounded-lg">
-                        <div class="row">
-                            <div class="form-group col-md-3 form1">
-                                <label>Choose Class </label>
-                            </div>
-                            <div class="accordion col-md-9 form1">
-                                <select class="form-control choosedClass" id="c_{{$class->id}}" name="class_id[]"
-                                    required>
-                                    <option value="">Choose Class</option>
-                                    @foreach($classes as $_class)
-                                    @if(count($_class->sharelesson) > 0)
-                                    @foreach($_class->sharelesson as $_sh)
-                                    @if($class->id == $_sh->class_id)
-                                    <option value="{{$_class->id}}" selected="selected">{{$_class->name}}</option>
-                                    @break
-                                    @endif
-                                    @endforeach
-                                    @else
-                                    <option value="{{$_class->id}}">{{$_class->name}}</option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="form-group col-md-3 student_{{$class->id}}">
-                                <label>Students </label>
-                            </div>
-                            <div class="form-group col-md-9 form1 student_{{$class->id}}">
-                                <select class="form-control select2" multiple name="class_students[]" required>
-                                    @foreach(
-                                    \App\ClassesStudent::with('student')->where([['class_id',$class->id],['teacher_id' ,
-                                    auth()->user()->id ]])->get() as $c_student)
-                                    @php ($data = [])
-                                    @foreach( $class->sharelesson as $share_student)
-                                    @if($c_student->student->id == $share_student->student->id )
-                                    {{$data[]=$share_student->student->id}}
-                                    <option value="{{$class->id}}_{{$c_student->student->id}}" selected="selected">
-                                        {{$c_student->student->fname}} {{$c_student->student->lname}} </option>
-                                    @break
-                                    @endif
-                                    @endforeach
-                                    @if(!in_array( $c_student->student->id, $data))
-                                    <option value="{{$class->id}}_{{$c_student->student->id}}">
-                                        {{$c_student->student->fname}} {{$c_student->student->lname}} </option>
-                                    @endif
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    @endif
-
-                    @endforeach
-                    @else
-                    <div class="form-group col-md-3 form1">
-                        <label>Choose Class </label>
-                    </div>
-                    <div class="accordion col-md-9 form1">
-                        <select class="form-control select2 choosedClass" id="c_1" name="class_id[]" required>
-                            @if($classes)
-                            <option value="">Choose Class</option>
+                        <!-- End Lesson Name -->
+                        <div id="all_classes">
+                        @if(count($share) > 0)
                             @foreach($classes as $class)
-                            <option value="{{$class->id}}">{{$class->name}}</option>
+                                @if(count($class->sharelesson) > 0)
+                                <div class="shadow rounded p-3" id="main_{{$class->id}}">
+                                    <div class="chooseClass">
+                                            <div class="form-group row">
+                                                <label for="lessonName" class="col-md-3 col-form-label">Choose Class</label>
+                                                <div class="col-md-7">
+                                                    <select class="form-control choosedClass" id="{{$class->id}}" required>
+                                                    @if($classes)
+                                                        <option value="">Choose Class</option>
+                                                        @foreach($classes as $_class)
+                                                        @if(count($_class->sharelesson) > 0)
+                                                        @foreach($_class->sharelesson as $_sh)
+                                                        @if($class->id == $_sh->class_id)
+                                                        <option value="{{$_class->id}}" selected="selected">{{$_class->name}}</option>
+                                                        @break
+                                                        @endif
+                                                        @endforeach
+                                                        @else
+                                                        <option value="{{$_class->id}}">{{$_class->name}}</option>
+                                                        @endif
+                                                        @endforeach
+                                                    @endif
+                                                    </select>
+                                                </div>
+                                                <div class=" col-md-2">
+                                                    <div class="lessonBtn">
+                                                        <i class="fa fa-eye mr-2 show" status="1" id="{{$class->id}}" style="cursor: pointer" aria-hidden="true"></i>
+                                                        <i class="fa fa-minus deleteDivClass" id="{{$class->id}}" style="cursor: pointer" aria-hidden="true"></i>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                    </div>
+                                    <div class="students_{{$class->id}}">
+                                        <div class="form-group row w-100 d-flex">
+                                            <label for="lessonName" class="col-md-3 col-form-label">Students</label>
+                                            <div class="col-sm-9" id="student_{{$class->id}}">
+                                            <select class="form-control select2" multiple name="class_students[]" required>
+                                                @foreach(
+                                                \App\ClassesStudent::with('student')->where([['class_id',$class->id],['teacher_id' ,
+                                                auth()->user()->id ]])->get() as $c_student)
+                                                @php ($data = [])
+                                                @foreach( $class->sharelesson as $share_student)
+                                                @if($c_student->student->id == $share_student->student->id )
+                                                {{$data[]=$share_student->student->id}}
+                                                <option value="{{$class->id}}_{{$c_student->student->id}}" selected="selected">
+                                                    {{$c_student->student->fname}} {{$c_student->student->lname}} </option>
+                                                @break
+                                                @endif
+                                                @endforeach
+                                                @if(!in_array( $c_student->student->id, $data))
+                                                <option value="{{$class->id}}_{{$c_student->student->id}}">
+                                                    {{$c_student->student->fname}} {{$c_student->student->lname}} </option>
+                                                @endif
+                                                @endforeach
+                                            </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endif
                             @endforeach
-                            @endif
-                        </select>
+                        @else
+                        <div class="shadow rounded p-3" id="main_1">
+                            <div class="chooseClass">
+                                    <div class="form-group row">
+                                        <label for="lessonName" class="col-md-3 col-form-label">Choose Class</label>
+                                        <div class="col-md-7">
+                                            <select class="form-control choosedClass" id="1">
+                                            @if($classes)
+                                                <option value="">Choose Class</option>
+                                                @foreach($classes as $class)
+                                                <option value="{{$class->id}}">{{$class->name}}</option>
+                                                @endforeach
+                                            @endif
+                                            </select>
+                                        </div>
+                                        <div class=" col-md-2">
+                                            <div class="lessonBtn">
+                                                <i class="fa fa-eye mr-2 show" status="1" id="1" style="cursor: pointer" aria-hidden="true"></i>
+                                                <i class="fa fa-minus deleteDivClass" id="1" style="cursor: pointer" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <div class="students_1" style="display:none;">
+                                <div class="form-group row w-100 d-flex">
+                                    <label for="lessonName" class="col-md-3 col-form-label">Students</label>
+                                    <div class="col-sm-9" id="student_1">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                        </div>
+                        <div class="col-md-12 mt-5">
+                            <div class="accordion d-flex justify-content-between">
+                                <i class="fa fa-plus add_new_class" aria-hidden="true"
+                                    style="margin-top: 10px;cursor: pointer;"></i>
+                                <button type="submit" class="btn btn-primary">Save Share</button>
+                            </div>
+                        </div>
                     </div>
-                    @endif
-
+                    </form>
                 </div>
-
-                <div class="col-md-12 mt-5">
-                    <div class="accordion d-flex justify-content-between">
-                        <i class="fa fa-plus add_new_class" aria-hidden="true"
-                            style="margin-top: 10px;cursor: pointer;"></i>
-                        <button type="submit" class="btn btn-primary">Save Share</button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
@@ -141,58 +150,7 @@
 
 
 @endsection
-<div class="shareLesson mt-5">
-    <div class="container bg-white rounded py-4">
-        <h4 class="h4 text-primary"> Share the lessons </h4>
-        <form method="post" action="{{route('saveShare')}}" enctype="multipart/form-data">
-            <!-- begin Lesson Name -->
-            <div class="form-group row">
-                <label for="lessonName" class="col-md-3 col-form-label">Lesson Name</label>
-                <div class="col-sm-9 d-flex align-content-center">
-                    <span> Lorem ipsum. </span>
-                    <input type="hidden" id="lesson_id" value="356" class="form-control" name="lesson_id">
-                </div>
-            </div>
-            <!-- End Lesson Name -->
-            <div class="shadow rounded p-3">
-            <div class="chooseClass">
-                    <div class="form-group row">
-                        <label for="lessonName" class="col-md-3 col-form-label">Choose Class</label>
-                        <div class="col-md-7">
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                            </select>
-                        </div>
-                        <div class=" col-md-2">
-                            <div class="lessonBtn">
-                                <button class="btn ">
-                                    <i class="fa fa-eye" aria-hidden="true"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="fa fa-minus" aria-hidden="true"></i>
-                                </button>
-                            </div>
-                        </div>
-                </div>
-            </div>
-            <div class="Students">
-                <div class="form-group row w-100 d-flex">
-                    <label for="lessonName" class="col-md-3 col-form-label">Students</label>
-                    <div class="col-sm-9">
-                        <select class="form-control select2" multiple>
-                            <option>1</option>
-                            <option>2</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            </div>
-    </div>
-    </form>
 
-</div>
-</div>
 @section('scripts')
 <script src="{{ url('js/add_class.js')}}"></script>
 @endsection
