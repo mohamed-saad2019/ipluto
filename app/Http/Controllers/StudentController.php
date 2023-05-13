b60d44c1ae30b38f91063e07ee8a9416be42e16b
Nasr Khaled <n.khaled@abgegypt.com>
2023-05-13 13:05:18 -0700

    from nasr last update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -73,9 +73,9 @@ class StudentController extends Controller
 
           $class_share    = ShareLessons::where('student_id',\Auth::user()->id)->pluck('class_id');
          
-       return   $class_active   =  \DB::table('classes_student')->whereIn('class_id',$class_share)->where('student_id',\Auth::user()->id)->where('status','1')->pluck('class_id');
+          $class_active   =  \DB::table('classes_student')->whereIn('class_id',$class_share)->where('student_id',\Auth::user()->id)->where('status','1')->pluck('class_id');
  
-          $instructor_sub = ShareLessons::where('student_id',\Auth::user()->id)->whereIn('class_id',$class_active);
+         return $instructor_sub = ShareLessons::where('student_id',\Auth::user()->id)->whereIn('class_id',$class_active)->pluck('instructor_id');
  
           $teacher_lesson = InstructorsSubjects::where('subject_id',request('subject_id'))
           ->whereIN('instructor_id',$instructor_sub->pluck('instructor_id')->toArray());
