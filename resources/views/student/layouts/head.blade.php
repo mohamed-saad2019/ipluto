9c5dbf90bd9ac8607f1363c3a334bb1d57f28a6d
Nasr Khaled <n.khaled@abgegypt.com>
2023-05-13 10:00:57 -0700

    from nasr last update

--- resources/views/student/layouts/head.blade.php
+++ resources/views/student/layouts/head.blade.php
@@ -157,6 +157,9 @@
                                                     <h5>{{\Auth::user()->fname.' '.\Auth::user()->lname}}</h5>
                                                     <h6 style="margin-top:-8px !important;">{{\Auth::user()->email}}
                                                     </h6>
+                                                    <h6 style="margin-top:-8px !important;">
+                                                        {{\Auth::user()->code}}
+                                                    </h6>
 
                                                 </div>
 
